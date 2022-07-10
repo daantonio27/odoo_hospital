cad9d1df244d64c073ab24e6295202b85a33966c
Da Antonio <daantonioc3@gmail.com>
2022-07-10 14:59:27 +0000

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital management system',
+    'description': """Hospital management system""",
+    'depends': ['mail', 'product',],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/om_hospital_data.xml',
+        'wizard/cancel_appointment_view.xml',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/patient_tag_view.xml',
+        'views/appointment_view.xml',
+        'views/female_patient_view.xml',
+        'views/res_config_settings_views.xml',
+        'reports/patient_card.xml',
+        'reports/report.xml'
+        ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
